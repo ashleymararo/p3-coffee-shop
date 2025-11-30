3ab24a940275f29db19977248babead4adb3d7d6
ashleymararo <ashleymararo222@gmail.com>
2025-12-01 01:48:15 +0300

    feat: add num_orders and average_price to coffee

--- coffee.py
+++ coffee.py
@@ -41,3 +41,12 @@ class Coffee:
 
     def customers(self):
         return list(set(order.customer for order in self.orders()))
+
+    def num_orders(self):
+        return len(self.orders())
+
+    def average_price(self):
+        orders = self.orders()
+        if not orders:
+            return 0
+        return sum(order.price for order in orders) / len(orders)
