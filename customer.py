28aa0e0d6818947e5f42e2ea5be945cabf1f52bb
ashleymararo <ashleymararo222@gmail.com>
2025-12-01 01:36:52 +0300

    feat: add customer initializer and name validation

--- customer.py
+++ customer.py
@@ -13,3 +13,15 @@ class Customer:
         if not (1 <= len(value) <= 15):
             raise Exception("Name must be between 1 and 15 characters.")
         self._name = value
+
+from order import Order
+
+    def orders(self):
+        return [order for order in Order.all if order.customer == self]
+
+    def coffees(self):
+        return list({order.coffee for order in self.orders()})
+
+    def create_order(self, coffee, price):
+        return Order(self, coffee, price)
+
