54761b478e58dae64a40f2422bcc0beb86958d9e
ashleymararo <ashleymararo222@gmail.com>
2025-12-01 01:41:47 +0300

    feat: implement order initializer with validations

--- order.py
+++ order.py
@@ -2,45 +2,33 @@ from customer import Customer
 from coffee import Coffee
 
 class Order:
-    all = []
-
     def __init__(self, customer, coffee, price):
-        self.customer = customer
-        self.coffee = coffee
-        self.price = price
-        Order.all.append(self)
+        # Validate customer
+        if not isinstance(customer, Customer):
+            raise Exception("customer must be a Customer instance")
 
-    # customer property
+        # Validate coffee
+        if not isinstance(coffee, Coffee):
+            raise Exception("coffee must be a Coffee instance")
+
+        # Validate price
+        if not isinstance(price, (int, float)):
+            raise Exception("price must be a number")
+        if price < 1.0 or price > 10.0:
+            raise Exception("price must be between 1.0 and 10.0")
+
+        self._customer = customer
+        self._coffee = coffee
+        self._price = float(price)
+
     @property
     def customer(self):
         return self._customer
 
-    @customer.setter
-    def customer(self, value):
-        if not isinstance(value, Customer):
-            raise Exception("customer must be a Customer instance")
-        self._customer = value
-
-    # coffee property
     @property
     def coffee(self):
         return self._coffee
 
-    @coffee.setter
-    def coffee(self, value):
-        if not isinstance(value, Coffee):
-            raise Exception("coffee must be a Coffee instance")
-        self._coffee = value
-
-    # price property
     @property
     def price(self):
         return self._price
-
-    @price.setter
-    def price(self, value):
-        if not isinstance(value, (int, float)):
-            raise Exception("price must be a number")
-        if not 1.0 <= value <= 10.0:
-            raise Exception("price must be between 1.0 and 10.0")
-        self._price = float(value)
